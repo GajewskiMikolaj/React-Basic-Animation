2e5604554f7b74aa726b3e6c0907b987ee157867
Mikołaj G <gajowymikolaj@gmail.com>
2023-11-16 21:44:03 +0100

    React basic animation App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import Animated from './Animated';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Animated
+        className="flying-cloud"
+        src="./cloud.png"
+        ratioX="0.35"
+        ratioY="0.32"
+        />
+        <Animated
+        className="flying-cloud"
+        src="./cloud.png"
+        ratioX="0.25"
+        ratioY="0.12"
+        />
+        <Animated
+        className="flying-cloud"
+        src="./cloud.png"
+        ratioX="0.25"
+        ratioY="0.52"
+        />
+        <Animated
+        className="flying-cloud"
+        src="./cloud.png"
+        ratioX="0.15"
+        ratioY="0.22"
+        />
+        <Animated
+        className="flying-cloud"
+        src="./cloud.png"
+        ratioX="0.15"
+        ratioY="0.12"
+        />
+        <Animated
+        className="flying-cloud"
+        src="./cloud.png"
+        ratioX="0.25"
+        ratioY="0.12"
+        />
+        <Animated
+        className="flying-cloud"
+        src="./cloud.png"
+        ratioX="0.5"
+        ratioY="0.1"
+        />
+        <Animated
+        className="flying-cat"
+        src="./cat.png"
+        ratioX="0.35"
+        ratioY="0.32"
+        />
       </header>
     </div>
   );
